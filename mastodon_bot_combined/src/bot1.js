26e2d37f7fbd98c7771a6d6ccc1b777a6f878609
Nathan Graule <solarliner@gmail.com>
2018-10-06 19:10:27 +0200

    Implement async/await in first bot

--- mastodon_bot_combined/src/bot1.js
+++ mastodon_bot_combined/src/bot1.js
@@ -1,25 +1,32 @@
 const M = require("./mastodon");
 
-function toot() {
+async function toot() {
   const num = Math.floor(Math.random() * 100);
   const params = {
     spoiler_text: "The meaning of life is: ",
     status: num
   }
 
-  M.post('statuses', params, (error, data) => {
-    if (error) {
-      console.error(error);
-    } else {
-      //fs.writeFileSync('data.json', JSON.stringify(data, null, 2));
-      //console.log(data);
-      console.log(`ID: ${data.id} and timestamp: ${data.created_at}`);
-      console.log(data.content);
-    }
-  });
+  // M.post('statuses', params, (error, data) => {
+  //   if (error) {
+  //     console.error(error);
+  //   } else {
+  //     //fs.writeFileSync('data.json', JSON.stringify(data, null, 2));
+  //     //console.log(data);
+  //     console.log(`ID: ${data.id} and timestamp: ${data.created_at}`);
+  //     console.log(data.content);
+  //   }
+  // });
+  const data = await M.post('statuses', params);
+  console.log(`ID: ${data.id} and timestamp: ${data.created_at}`);
+  console.log(data.content);
 }
 
-module.exports = function bot1() {
+module.exports = async function bot1() {
+  try {
     toot();
     setInterval(toot, 24 * 60 * 60 * 1000);
+  } catch (err) {
+    console.error(err);
+  }
 }
